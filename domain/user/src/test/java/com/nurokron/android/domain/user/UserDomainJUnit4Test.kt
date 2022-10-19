7f9cdcb79fce2443bc2aa14f15085385c411263e
Florian Lamotte <florian.lamotte@gmail.com>
2022-10-19 12:08:48 +0100

    Introduced more tests

--- domain/user/src/test/java/com/nurokron/android/domain/user/UserDomainJUnit4Test.kt
+++ domain/user/src/test/java/com/nurokron/android/domain/user/UserDomainJUnit4Test.kt
@@ -1,13 +1,13 @@
-package com.nurokron.android.data.userrepository
+package com.nurokron.android.domain.user
 
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
-internal class UserRepositoryJUnit4Test {
+internal class UserDomainJUnit4Test {
 
     @Test
     fun `assert something is true`() {
-        assertTrue(true)
+        assertTrue(false)
     }
 
 }
