5090347f8039ddbe6dd7b6468d9722536681ad51
Florian Lamotte <florian.lamotte@gmail.com>
2022-10-24 18:20:49 +0100

    Add tests to app

---
+++ app/src/test/java/com/nurokron/android/daggermultimodule/AppJUnit4Test.kt
@@ -0,0 +1,13 @@
+package com.nurokron.android.daggermultimodule
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class AppJUnit4Test {
+
+    @Test
+    fun `assert something is true`() {
+        assertTrue(true)
+    }
+
+}
---
+++ app/src/test/java/com/nurokron/android/daggermultimodule/AppJUnit5Test.kt
@@ -0,0 +1,13 @@
+package com.nurokron.android.daggermultimodule
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class AppJUnit5Test {
+
+    @Test
+    fun `assert something is true`() {
+        assertTrue(false)
+    }
+
+}
